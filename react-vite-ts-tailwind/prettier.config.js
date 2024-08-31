efae45aa4533162003fcb492a6aa9e6a38bff30c
Bryan Acosta <bryan-ax@hotmail.com>
2024-08-31 17:22:10 -0500

    style: add prettier and format all files

---
+++ react-vite-ts-tailwind/prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+    tabWidth: 4,
+    printWidth: 120,
+    endOfLine: "auto",
+    trailingComma: "es5",
+    arrowParens: "avoid",
+};
+
+export default config;
--- react-vite-ts-tailwind/src/App.tsx
+++ react-vite-ts-tailwind/src/App.tsx
@@ -1,12 +1,11 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
     return (
         <div className="w-screen h-screen p-6 flex justify-center items-center dark:bg-black">
-            <main
-                className="relative py-16 px-20 mb-16 shadow-lg shadow-cyan-600/30 border-2 border-b-0 border-cyan-200/50 rounded-xl overflow-hidden">
+            <main className="relative py-16 px-20 mb-16 shadow-lg shadow-cyan-600/30 border-2 border-b-0 border-cyan-200/50 rounded-xl overflow-hidden">
                 <div className="flex flex-col items-center gap-12">
                     <div className="flex justify-center items-center gap-4 ps-1">
                         <img
@@ -28,9 +27,11 @@ function App() {
                         />
                     </div>
                     <div className="text-center">
-                        <button type="button"
-                                onClick={() => setCount((count) => count + 1)}
-                                className="px-6 py-2 text-white font-medium text-sm rounded-lg shadow-lg shadow-cyan-500/50 bg-gradient-to-br from-cyan-500 to-blue-500 hover:bg-gradient-to-l">
+                        <button
+                            type="button"
+                            onClick={() => setCount(count => count + 1)}
+                            className="px-6 py-2 text-white font-medium text-sm rounded-lg shadow-lg shadow-cyan-500/50 bg-gradient-to-br from-cyan-500 to-blue-500 hover:bg-gradient-to-l"
+                        >
                             count is {count}
                         </button>
                         <p className="my-4 text-sm text-gray-700 dark:text-gray-100">
@@ -40,33 +41,57 @@ function App() {
                 </div>
 
                 <div className="absolute pointer-events-none left-[7%] top-0 opacity-50">
-                    <svg width="1237" height="405" viewBox="0 0 1237 405" fill="none"
-                         xmlns="http://www.w3.org/2000/svg">
+                    <svg
+                        width="1237"
+                        height="405"
+                        viewBox="0 0 1237 405"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
                         <g filter="url(#filter1)">
                             <ellipse cx="618.5" cy="-213" rx="268.5" ry="268" fill="#48DCFF"></ellipse>
                         </g>
                         <defs>
-                            <filter id="filter1" x="0" y="-831" width="1237" height="1236"
-                                    filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                <feFlood floodOpacity="0"/>
-                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                                <feGaussianBlur stdDeviation="175"/>
+                            <filter
+                                id="filter1"
+                                x="0"
+                                y="-831"
+                                width="1237"
+                                height="1236"
+                                filterUnits="userSpaceOnUse"
+                                colorInterpolationFilters="sRGB"
+                            >
+                                <feFlood floodOpacity="0" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                                <feGaussianBlur stdDeviation="175" />
                             </filter>
                         </defs>
                     </svg>
                 </div>
                 <div className="absolute pointer-events-none right-[7%] top-0 opacity-50">
-                    <svg width="1237" height="405" viewBox="0 0 1237 405" fill="none"
-                         xmlns="http://www.w3.org/2000/svg">
+                    <svg
+                        width="1237"
+                        height="405"
+                        viewBox="0 0 1237 405"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
                         <g filter="url(#filter2)">
                             <ellipse cx="618.5" cy="-213" rx="268.5" ry="268" fill="#3935FB"></ellipse>
                         </g>
                         <defs>
-                            <filter id="filter2" x="0" y="-831" width="1237" height="1236"
-                                    filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                <feFlood floodOpacity="0"/>
-                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                                <feGaussianBlur stdDeviation="175"/>
+                            <filter
+                                id="filter2"
+                                x="0"
+                                y="-831"
+                                width="1237"
+                                height="1236"
+                                filterUnits="userSpaceOnUse"
+                                colorInterpolationFilters="sRGB"
+                            >
+                                <feFlood floodOpacity="0" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                                <feGaussianBlur stdDeviation="175" />
                             </filter>
                         </defs>
                     </svg>
@@ -74,14 +99,21 @@ function App() {
                 <div className="absolute pointer-events-none bottom-0 left-1/2 -translate-x-1/2">
                     <svg width="702" height="2" viewBox="0 0 702 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g filter="url(#filter3)">
-                            <rect width="700" height="2" transform="matrix(-1 0 0 1 701 1)" fill="url(#gradient1)"/>
+                            <rect width="700" height="2" transform="matrix(-1 0 0 1 701 1)" fill="url(#gradient1)" />
                         </g>
                         <defs>
-                            <filter id="filter3" x="0" y="0" width="702" height="4"
-                                    filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                <feFlood floodOpacity="0"/>
-                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix"/>
-                                <feGaussianBlur stdDeviation="0.5"/>
+                            <filter
+                                id="filter3"
+                                x="0"
+                                y="0"
+                                width="702"
+                                height="4"
+                                filterUnits="userSpaceOnUse"
+                                colorInterpolationFilters="sRGB"
+                            >
+                                <feFlood floodOpacity="0" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" />
+                                <feGaussianBlur stdDeviation="0.5" />
                             </filter>
                             <linearGradient id="gradient1" x1="0" y1="1" x2="700" y2="1" gradientUnits="userSpaceOnUse">
                                 <stop stopColor="#38BDF8" stopOpacity="0"></stop>
@@ -95,14 +127,21 @@ function App() {
                 <div className="absolute pointer-events-none bottom-0 left-1/2 -translate-x-1/2">
                     <svg width="708" height="5" viewBox="0 0 708 5" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g filter="url(#filter4)">
-                            <rect width="700" height="2" transform="matrix(-1 0 0 1 704 4)" fill="url(#gradient2)"/>
+                            <rect width="700" height="2" transform="matrix(-1 0 0 1 704 4)" fill="url(#gradient2)" />
                         </g>
                         <defs>
-                            <filter id="filter4" x="0" y="0" width="708" height="10"
-                                    filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                <feFlood floodOpacity="0"/>
-                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix"/>
-                                <feGaussianBlur stdDeviation="2"/>
+                            <filter
+                                id="filter4"
+                                x="0"
+                                y="0"
+                                width="708"
+                                height="10"
+                                filterUnits="userSpaceOnUse"
+                                colorInterpolationFilters="sRGB"
+                            >
+                                <feFlood floodOpacity="0" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" />
+                                <feGaussianBlur stdDeviation="2" />
                             </filter>
                             <linearGradient id="gradient2" x1="0" y1="1" x2="700" y2="1" gradientUnits="userSpaceOnUse">
                                 <stop stopColor="#38BDF8" stopOpacity="0"></stop>
@@ -118,28 +157,43 @@ function App() {
             <div className="absolute pointer-events-none top-0 w-full h-full opacity-20 transform rotate-180">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1280 1008" className="w-full h-full">
                     <g filter="url(#filter5)">
-                        <path transform="rotate(5.378 -555.062 600)" fill="url(#gradient3)"
-                              d="M-555.062 600h2166.28v1045h-2166.28z"/>
+                        <path
+                            transform="rotate(5.378 -555.062 600)"
+                            fill="url(#gradient3)"
+                            d="M-555.062 600h2166.28v1045h-2166.28z"
+                        />
                     </g>
                     <defs>
-                        <linearGradient id="gradient3" x1="446.248" y1="2186" x2="1151.32" y2="235.034"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stopColor="#4FC3F7"/>
-                            <stop offset=".898" stopColor="#81D4FA" stopOpacity=".64"/>
-                            <stop offset="1" stopColor="#fff"/>
+                        <linearGradient
+                            id="gradient3"
+                            x1="446.248"
+                            y1="2186"
+                            x2="1151.32"
+                            y2="235.034"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stopColor="#4FC3F7" />
+                            <stop offset=".898" stopColor="#81D4FA" stopOpacity=".64" />
+                            <stop offset="1" stopColor="#fff" />
                         </linearGradient>
-                        <filter id="filter5" x="-1253" y="0" width="3454.68" height="2443.42"
-                                filterUnits="userSpaceOnUse"
-                                colorInterpolationFilters="sRGB">
-                            <feFlood floodOpacity="0"/>
-                            <feBlend in="SourceGraphic" in2="BackgroundImageFix"/>
-                            <feGaussianBlur stdDeviation="300"/>
+                        <filter
+                            id="filter5"
+                            x="-1253"
+                            y="0"
+                            width="3454.68"
+                            height="2443.42"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" />
+                            <feBlend in="SourceGraphic" in2="BackgroundImageFix" />
+                            <feGaussianBlur stdDeviation="300" />
                         </filter>
                     </defs>
                 </svg>
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
