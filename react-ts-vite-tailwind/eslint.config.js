b665dd157a67ac142bf20d3018f5d3d34dd42ae4
Bryan Acosta <bxacosta@hotmail.com>
2024-10-19 02:41:21 -0500

    chore: add prettier eslint config and configure some react rules

--- react-ts-vite-tailwind/eslint.config.js
+++ react-ts-vite-tailwind/eslint.config.js
@@ -4,11 +4,12 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    settings: { react: { version: "18.3" } },
+    settings: { react: { version: "detect" } },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
@@ -21,9 +22,15 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
+      ...react.configs.flat.recommended.rules,
+      ...react.configs.flat["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-expressions": ["error", { allowTernary: true }],
+      "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   }
